f02541242f4ecafc384627ee8153c353cc159977
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-02-12 08:48:35 +0800

    T

--- application/controllers/Login_cont.php
+++ application/controllers/Login_cont.php
@@ -31,7 +31,7 @@ class Login_cont extends CI_Controller
             $this->session->set_userdata('user_id', $user->id);
             $this->session->set_userdata('username', $user->username);
 
-            echo json_encode(['success' => true, 'redirect' => site_url('dashboard')]);
+            echo json_encode(['success' => true, 'redirect' => 'View_ui_cont/dashboard']);
         } else {
             echo json_encode(['success' => false, 'message' => 'Invalid username/email or password.']);
         }
