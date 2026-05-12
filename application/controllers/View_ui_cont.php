bb0e081e7fb649de988ab13fa867e417656473b4
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-05-12 08:53:56 +0800

    T

--- application/controllers/View_ui_cont.php
+++ application/controllers/View_ui_cont.php
@@ -11,9 +11,9 @@ class View_ui_cont extends CI_Controller
         date_default_timezone_set('Asia/Manila');
         $this->db->query("SET time_zone = '+08:00'");
 
-        if ($this->session->userdata('username') === "Admin") {
-            redirect('maintenance');
-        }
+        // if ($this->session->userdata('username') === "Admin") {
+        //     redirect('maintenance');
+        // }
 
         if (!$this->session->userdata('logged_in')) {
             redirect('login');
